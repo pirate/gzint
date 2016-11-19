85f4373c58cd619224748b19d0a542861c8a54e5
Nick Sweeting <git@nicksweeting.com>
2016-11-19 14:46:34 -0500

    add math and comparison tests on real huge ints

--- gzint/tests.py
+++ gzint/tests.py
@@ -1,24 +1,40 @@
 import unittest
 
-from gzint import HugeInt
+try:
+    from .main import HugeInt
+except SystemError:
+    print('''Tests must be run using:
+    python -m gzint.tests
+    or
+    python setup.py test''')
+    raise SystemExit(1)
 
 
 class TestHugeInt(unittest.TestCase):
 
+    # share one copy of these between all tests for speed reasons
+    large_int = 10**1000000
+    large = HugeInt(large_int)
+
     def test_init(self):
-        test_int = HugeInt()
-        assert not test_int._is_huge
-        assert test_int._value == 0
-        assert test_int == 0
-        assert str(test_int) == '0'
-        assert repr(test_int) == '0'
+        zero = HugeInt()
+        assert not zero._is_huge
+        assert zero._value == 0
+        assert str(zero) == '0'
+        assert repr(zero) == '0'
+
+        assert self.large._is_huge
+        assert str(self.large) == str(self.large_int)
+        assert repr(self.large) == '1000000000000000...(1000001)'
 
     def test_comparisons(self):
-        assert HugeInt(10) == HugeInt(10)
+        assert HugeInt(0) == 0
         assert HugeInt(10) == 10
+        assert HugeInt(10) == HugeInt(10)
         assert 10 == HugeInt(10)
-        assert HugeInt(10**10000) == 10**10000
-        assert HugeInt(10**1000000) == 10**1000000
+
+        assert self.large == self.large
+        assert self.large == self.large_int
 
     def test_identity(self):
         huge_ints = {HugeInt(5), HugeInt(6), 7}
@@ -33,6 +49,7 @@ class TestHugeInt(unittest.TestCase):
         assert HugeInt(10) + HugeInt(10) == 20
         assert HugeInt(10) + HugeInt(10) == HugeInt(20)
         assert HugeInt(20) / HugeInt(10) == 2
+        assert self.large + 1 == self.large_int + 1
 
 
 if __name__ == '__main__':
